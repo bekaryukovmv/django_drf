fc6625a624bc6c05c129849f96d472c07e8743c6
Maxim Bekaryukov <bekaryukovmv@gmail.com>
2020-01-24 18:11:38 +0700

    ch 4 and 5

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,41 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetList, SnippetDetail
+from snippets import views
+
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'),
+    path('snippets/<int:pk>/',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    path('users/',
+        views.UserList.as_view(),
+        name='user-list'),
+    path('users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail')
+])
 
 
-urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-]
+# from django.urls import path
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from .views import SnippetList, SnippetDetail, UserList, UserDetail, SnippetHighlight, api_root
+
+
+# urlpatterns = [
+#     path('snippets/', SnippetList.as_view()),
+#     path('snippets/<int:pk>/', SnippetDetail.as_view()),
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('', api_root),
+#     path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view()),
+# ]
 
 # Добавляет суффиксы форматов (расширения)
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
